2f3fef66b2e14b56ff2c7932478228b393cfc8a2
Felipe Capiberibe <100447547+FelipeCapiberibe@users.noreply.github.com>
2022-11-16 22:07:37 -0300

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+// console
+console.log("Teste"); 
+console.error("TesteError"); 
+console.warn("Teste Aviso");
+
+// variáveis: são formas de guardar algum valor na memoria para uso posterior no nosso software
+var x = 10; 
+let y =  15; 
+const z = 20;
+
+if (1) {
+    let y = 55    
+    console.log(y);
+}; 
+console.log(x); 
+console.log(y);
+console.log(z);
+
+// tipos de dados
+const nome = "felipe"; 
+console.log(nome);
+console.log( typeof nome); // o typeof exibe o tipo de dado 
+
