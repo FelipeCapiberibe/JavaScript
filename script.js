e5d61e9b45c3edb15ca5535eca3282462fc6ffc4
Felipe Capiberibe <100447547+FelipeCapiberibe@users.noreply.github.com>
2022-11-19 15:16:22 -0300

    update

--- script.js
+++ script.js
@@ -29,3 +29,32 @@ console.log( typeof linguagens )
 const user = { email: "luizfelipe@teste.com" , password: "teste12345" , idade: 30 } //object literals 
 console.log( user )
 console.log( typeof user )
+
+// métodos de manipulação de string. 
+const nomeTodo = "Luiz Felipe"; 
+console.log(nomeTodo.length); // length dá uma quantidade de caracters numa string. 
+ 
+const stringToArray = nomeTodo.split(' '); 
+console.log(stringToArray); 
+
+console.log(nomeTodo.toLowerCase()); // transforma o nome atribuido a minha variavel em letras minusculas.
+console.log(nomeTodo.toUpperCase()); // transoforma em letras maiusculas.
+
+console.log(nomeTodo.slice(0, 4)); 
+
+// métodos de manipulação de array 
+const lista = ['a', 'b', 'c', 'd', 'e', 'f']; 
+console.log(lista.length); // quantidade de elementos na minha array. 
+
+console.log(lista[2]);   
+
+lista[5] = 'w'; // mudei o elemento a minha lista array.
+console.log(lista); 
+
+console.log(lista[lista.length - 1]) // pegando o ultimo elemento da lista array.
+
+lista.push('g'); // atribui um novo elemento a minha lista array.
+console.log(lista); 
+
+lista.pop(); // remover um elemento final da minha lista array.
+console.log(lista);
